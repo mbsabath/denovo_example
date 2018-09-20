4617d9905cb8ae26945e4cb34acd5ad8e9339bb5
mbsabath <bsabath1@gmail.com>
2018-09-20 10:47:32 -0400

    add sensitivity

--- run_denovo.R
+++ run_denovo.R
@@ -7,5 +7,8 @@ library(denovo)
 
 info <- read.csv("lalonde_matched.csv")
 
-out <- denovo(info)
+out <- denovo(info, split.ratio=0.25, total.significance=0.05, gamma=0.01))
 saveRDS(out, "denovo_out.rds")
+
+sensi <- denovo.sensi(info, Gamma.vec=c(1.01, 1.02, 1.05, 1.08), 
+			total.significance=0.05, gamma=0.0001)
